e0d200fb8a6767168680821364d1ffa67dabc878
jessica12579 <jessica.aranguren01@uptc.edu.co>
2021-11-12 10:06:23 -0500

    actualizacion

---
+++ cmake-build-debug/CMakeFiles/ejemploRepositorio.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ejemploRepositorio.dir/main.cpp.obj"
+  "ejemploRepositorio.exe"
+  "ejemploRepositorio.exe.manifest"
+  "ejemploRepositorio.pdb"
+  "libejemploRepositorio.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ejemploRepositorio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ejemploRepositorio.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ejemploRepositorio.
+# This may be replaced when dependencies are built.
